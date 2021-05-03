e8fe958d0757f93b297fe8c5717b37113b88ba4a
dokidia <42838268+twk1024@users.noreply.github.com>
2021-05-04 01:23:58 +0900

    KOI 2020 중등부 1차 실기
    2021/05/04

---
+++ hamburger.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    /* 문제: https://koi.or.kr/assets/koi/2020/1/problems/m1-problems.pdf */
+
+    int n,k; // n=식탁의 길이 k=햄버거를 선택가능한 거리
+    cin>>n>>k;
+
+    char a[n+1];
+    int b[n+1], peoplecnt=0, hamcnt=0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i]; // H(amburger) or P(eople)
+        
+        if(a[i]=='H')
+        {
+            b[i]=1; // 햄버거
+            hamcnt++;
+        }
+        else
+        {
+            b[i]=0; // 사람
+            peoplecnt++;
+        }
+            
+    }
+
+    int cnt=0;
+        
+    for(int j=0;j<n;j++)
+    {
+        if(b[j]==0) // 현재 순서가 사람일 경우
+        {
+            for(int f=j-k;f<=j+k;f++) // <<
+            {
+                if(j!=f)
+                {
+                    if(b[f]==1) // 현재 순서가 햄버거일 경우
+                    {
+                        b[f]=3; // 햄버거를 먹는다 (3)
+                        cnt++;
+                        break;
+                    }
+                }
+                    
+            }
+
+        }
+    }
+
+    cout<<cnt<<endl;
+
+    return 0;
+}
