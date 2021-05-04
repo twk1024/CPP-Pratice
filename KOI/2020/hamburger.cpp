ad322c18716e4756ce3c2d59110e20ed32fa0c1f
dokidia <42838268+twk1024@users.noreply.github.com>
2021-05-04 11:47:30 +0900

    Remove Unnecessary Codes
    Removed Some Unused Variables and Codes

--- KOI/2020/hamburger.cpp
+++ KOI/2020/hamburger.cpp
@@ -3,30 +3,18 @@ using namespace std;
 int main()
 {
     /* 
-     문제이름: 햄버거 분배
-     문제: https://koi.or.kr/assets/koi/2020/1/problems/m1-problems.pdf 
+      문제이름: 햄버거 분배
+      문제: https://koi.or.kr/assets/koi/2020/1/problems/m1-problems.pdf 
     */
 
-    int n,k; // n=식탁의 길이 k=햄버거를 선택가능한 거리
+    int n,k; // 식탁의 길이, 햄버거를 선택가능한 거리
     cin>>n>>k;
 
     char a[n+1];
-    int b[n+1], peoplecnt=0, hamcnt=0;
+    int b[n+1];
     for(int i=0;i<n;i++)
     {
         cin>>a[i]; // H(amburger) or P(eople)
-        
-        if(a[i]=='H')
-        {
-            b[i]=1; // 햄버거
-            hamcnt++;
-        }
-        else
-        {
-            b[i]=0; // 사람
-            peoplecnt++;
-        }
-            
     }
 
     int cnt=0;
@@ -45,10 +33,8 @@ int main()
                         cnt++;
                         break;
                     }
-                }
-                    
+                }       
             }
-
         }
     }
 
