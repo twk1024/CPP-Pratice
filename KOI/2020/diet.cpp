36681ba0b42f127b3ab60ae91dfe650bc5042aae
dokidia <42838268+twk1024@users.noreply.github.com>
2021-05-07 00:23:38 +0900

    KOI 2020 #2 Problem Another Bug Fix
    Completed the code

--- KOI/2020/diet.cpp
+++ KOI/2020/diet.cpp
@@ -2,22 +2,21 @@
 using namespace std;
 int main()
 {
-    
+
     /* 
       문제이름: 다이어트
-      문제: https://koi.or.kr/assets/koi/2020/1/problems/m1-problems.pdf 
+      문제: https://koi.or.kr/assets/koi/2020/1/problems/m2-problems.pdf
     */
-    
+
     int n; // 식재료의 개수
     cin>>n;
 
     int a,b,c,d; // 최소 영양성분
     cin>>a>>b>>c>>d;
 
-    int o[n], p[n], f[n], s[n], v[n], cst[n]; // 재료 순서, 단백질, 지방, 탄수화물, 비타민, 가격
+    int p[n], f[n], s[n], v[n], cst[n]; // 단백질, 지방, 탄수화물, 비타민, 가격
     for(int i=0;i<n;i++)
     {
-        o[i]=i;
         cin>>p[i]>>f[i]>>s[i]>>v[i]>>cst[i];
     }
 
@@ -27,10 +26,12 @@ int main()
     {
         for(int j=0;j<n;j++)
         {
+            cost=0;
+
             if(i!=j)
             {
                 cost=cst[i]+cst[j];
-                if(p[i]+p[j] > a && f[i]+f[j] > b && s[i]+s[j] > c && v[i]+v[j] > d)
+                if(p[i]+p[j] >= a && f[i]+f[j] >= b && s[i]+s[j] >= c && v[i]+v[j] >= d)
                 {
                     if(cost<leastcost)
                     {
@@ -45,10 +46,10 @@ int main()
 
                 for(int q=0;q<n;q++)
                 {
-                    if(q!=j)
+                    if(q!=j && q!=i && i!=j)
                     {
-                        cost=cost+cst[q];
-                        if(p[i]+p[j]+p[q] > a && f[i]+f[j]+f[q] > b && s[i]+s[j]+s[q] > c && v[i]+v[j]+v[q] > d)
+                        cost=cst[i]+cst[j]+cst[q];
+                        if(p[i]+p[j]+p[q] >= a && f[i]+f[j]+f[q] >= b && s[i]+s[j]+s[q] >= c && v[i]+v[j]+v[q] >= d)
                         {
                             if(cost<leastcost)
                             {
@@ -64,10 +65,10 @@ int main()
 
                         for(int k=0;k<n;k++)
                         {
-                            if(k!=q)
+                            if(q!=j && q!=i && i!=j && k!=q && k!=j && k!=i)
                             {
-                                cost=cost+cst[k];
-                                if(p[i]+p[j]+p[q]+p[k] > a && f[i]+f[j]+f[q]+f[k] > b && s[i]+s[j]+s[q]+s[k] > c && v[i]+v[j]+v[q]+v[k] > d)
+                                cost=cst[i]+cst[j]+cst[q]+cst[k];
+                                if(p[i]+p[j]+p[q]+p[k] >= a && f[i]+f[j]+f[q]+f[k] >= b && s[i]+s[j]+s[q]+s[k] >= c && v[i]+v[j]+v[q]+v[k] >= d)
                                 {
                                     if(cost<leastcost)
                                     {
@@ -76,10 +77,12 @@ int main()
                                         result[0]=q+1;
                                         result[1]=j+1;
                                         result[2]=i+1;
-                                        sort(result, result+3);
+                                        result[3]=k+1;
+                                        sort(result, result+4);
                                         break;
                                     }
                                 }
+
 
                             }
                         }
@@ -88,16 +91,16 @@ int main()
 
             }
         }
-        cost=0;
     }
 
     cout<<leastcost<<endl;
 
     for(int i=0;i<50;i++)
     {
-        if(result[i]==0)
+        if(result[i+1]==0)
         {
             break;
+
         }
 
         cout<<result[i]<<' ';
